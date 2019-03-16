ffa8d37cebacba2b1262a10b76e7ed16c1426c0a
Aiza Tilenbaeva <tilenbaevaa@gmail.com>
2019-03-16 11:56:56 -0500

    pullSCM edited

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,5 +1,5 @@
 node {
-    properties([parameters([string(defaultValue: '127.0.0.1', description: 'Please give IP to host a Website', name: 'DEVIP', trim: true)])]), properties([pipelineTriggers([pollSCM('* * * * *')])])
+    properties([parameters([string(defaultValue: '127.0.0.1', description: 'Please provide IP address to host a Website', name: 'DEVIP', trim: true)]), pipelineTriggers([pollSCM('* * * * *')])])
 
     stage ("Pull git") {
         git "git@github.com:tilenbaeva/Website.git"
